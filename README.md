a9956c0aadf7725fd389990c23a5098acc896f95
Ken Leung <damppuppy@kmb.local>
2015-01-12 02:20:36 -0800

    committing initial files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sudokuJS
 My attempt at a sudoku generator and solver
+
+Copyright 2015 Ken Leung
---
+++ sudoku_spec.js
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2015 Ken Leung
+ */
+describe("A test suite", function() {
+   beforeEach(function() { });
+   afterEach(function() { });
+   it('should fail', function() { expect(true).to.be.false; });
+});
